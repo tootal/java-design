91eadaf3dfee6beeaca333930b87ede7147c3a8f
tootal <tootal@yeah.net>
2021-03-24 12:58:35 +0800

    add decorator

---
+++ decorator/SideBorder.java
@@ -0,0 +1,21 @@
+package decorator;
+
+public class SideBorder extends Border {
+    private char borderChar;
+    public SideBorder(Display display, char ch) {
+        super(display);
+        borderChar = ch;
+    }
+    @Override
+    public int getColumns() {
+        return 1 + display.getColumns() + 1;
+    }
+    @Override
+    public int getRows() {
+        return display.getRows();
+    }
+    @Override
+    public String getRowText(int row) {
+        return borderChar + display.getRowText(row) + borderChar;
+    }
+}
