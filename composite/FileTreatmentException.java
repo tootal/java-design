8a452cd53eeb73d84b077a2a6868361910bf8346
tootal <tootal@yeah.net>
2021-03-24 12:36:39 +0800

    add composite

---
+++ composite/FileTreatmentException.java
@@ -0,0 +1,11 @@
+package composite;
+
+public class FileTreatmentException extends RuntimeException {
+    private static final long serialVersionUID = -3171118446645746351L;
+    public FileTreatmentException() {
+
+    }
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}
