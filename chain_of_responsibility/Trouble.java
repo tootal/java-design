e29ca64f2b186c011b9db1185bdc14eb88e53f61
tootal <tootal@yeah.net>
2021-03-24 15:19:51 +0800

    add chain of responsibility

---
+++ chain_of_responsibility/Trouble.java
@@ -0,0 +1,14 @@
+package chain_of_responsibility;
+
+public class Trouble {
+    private int number;
+    public Trouble(int number) {
+        this.number = number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public String toString() {
+        return "[Trouble " + number + "]";
+    }
+}
