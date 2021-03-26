cfccb8242fcdc6925b89697997c8b46e8debfbde
tootal <tootal@yeah.net>
2021-03-26 17:05:23 +0800

    add interpreter

---
+++ interpreter/ParseException.java
@@ -0,0 +1,9 @@
+package interpreter;
+
+public class ParseException extends Exception {
+    private static final long serialVersionUID = -2272853281279564680L;
+
+    public ParseException(String msg) {
+        super(msg);
+    }
+}
